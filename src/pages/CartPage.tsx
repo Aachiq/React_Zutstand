540811b5923fa920eb4a9317551f168166c1d088
Ecodexer <elhocine@ecodexia.com>
2026-08-04 15:06:46 +0100

    try first data in store hard & select items & show  in page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,7 +1,20 @@
+import { useCartStore } from "../store/cartStore";
+
 function CartPage() {
+  const cart = useCartStore((state) => state.cart);
+  console.log(cart);
+
   return (
     <div>
       <h1>Shopping Cart</h1>
+      {cart.map((item) => (
+        <div key={item.id}>
+          <h3>{item.title}</h3>
+          <p>${item.price}</p>
+          <p>Quantity : {item.quantity}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -8,7 +8,28 @@ interface CartStore {
 }
 
 export const useCartStore = create<CartStore>(() => ({
-  cart: [],
-
+  cart: [
+    {
+      id: 1,
+      title: "MacBook Pro",
+      price: 2500,
+      image: "https://picsum.photos/200?1",
+      quantity: 1,
+    },
+    {
+      id: 2,
+      title: "iPhone 16",
+      price: 1200,
+      image: "https://picsum.photos/200?2",
+      quantity: 2,
+    },
+    {
+      id: 3,
+      title: "AirPods Pro",
+      price: 300,
+      image: "https://picsum.photos/200?3",
+      quantity: 1,
+    },
+  ],
   addToCart: () => {}
 }));

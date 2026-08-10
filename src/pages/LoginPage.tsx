17b69885b06a07fd09c7bc7f18efddefe068d18d
Ecodexer <elhocine@ecodexia.com>
2026-08-10 11:04:58 +0100

    add first login action with api call

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,47 @@
+import { useState } from "react";
 import { useAuthStore } from "../store/authStore";
 
 function LoginPage() {
+  const login = useAuthStore(
+    (state) => state.login
+  );
 
-   const login = useAuthStore(state => state.login);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-   return (
-      <div>
-         <h2>Login</h2>
-         <button onClick={login}>Login</button>
-      </div>
-   );
+  const handleLogin = async () => {
+    try {
+      await login(email, password);
+
+      console.log("Login successful");
+    } catch (error) {
+      console.log("Login failed");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <button onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
 }
+
 export default LoginPage;
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,26 +1,49 @@
 import { create } from "zustand";
 import type { User } from "../types/auth";
-import { devtools } from "zustand/middleware";
 
 interface AuthStore {
    user: User | null;
-   login: () => void;
+   login: (email: string, password: string) => Promise<void>;
    logout: () => void;
    setUser: (user: User | null) => void;
 }
 export const useAuthStore = create<AuthStore>(
-  devtools(
     (set) => ({
-      user: {},
-      login: () => {
-        set({
-            user:{
-                id:1,
-                name:"Elhocine",
-                email:"elhocine@gmail.com"
+      user: null,
+      login: async (email, password) => {
+        const response = await fetch("http://localhost:3000/auth/login",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email,
+                    password,
+                }),
             }
-        })
+        );
+
+        if (!response.ok) {
+            throw new Error("Invalid credentials");
+        }
+        // backend login send this object without token becasue in backend there is res.cookie("tokanValue") with http-only:
+        // front won't use (Header+Authorization Bearer) -> it will use "credentials: "include" & backend will receive token with (req.coockie)
+        // req.cookies.tokanValue() not req.headers.authorization
+        // {
+        //     "user": {
+        //         "id": 1,
+        //         "name": "Elhocine",
+        //         "email": "test@test.com"
+        //     }
+        // }
+
+        const data = await response.json();
+
+        set({
+            user: data.user,
+        });
       },
+
       logout: () => {
         set({
             user: null
@@ -32,8 +55,5 @@ export const useAuthStore = create<AuthStore>(
         })
       }
     }),
-    {
-      name: "auth-store",
-    }
   )
-);
+;
